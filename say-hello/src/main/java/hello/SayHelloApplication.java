bd07b1e79d3deac1b6fb3e587f00cbbe23ebc8fd
Marcelo Wippel <mawippel2@hotmail.com>
2019-06-15 10:56:32 -0300

    feat: cleaning the project and testing

--- say-hello/src/main/java/hello/SayHelloApplication.java
+++ say-hello/src/main/java/hello/SayHelloApplication.java
@@ -1,13 +1,11 @@
 package hello;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
@@ -18,18 +16,13 @@ public class SayHelloApplication {
   @RequestMapping(value = "/greeting")
   public String greet() {
     log.info("Access /greeting");
-
-    List<String> greetings = Arrays.asList("Hi there", "Greetings", "Salutations");
-    Random rand = new Random();
-
-    int randomNum = rand.nextInt(greetings.size());
-    return greetings.get(randomNum);
+    return "Olá humano!";
   }
 
   @RequestMapping(value = "/")
   public String home() {
-    log.info("Access /");
-    return "Hi!";
+    log.info("Ribbon verificando a saúde da instância...");
+    return "Olá!";
   }
 
   public static void main(String[] args) {
--- user/src/main/java/hello/UserApplication.java
+++ user/src/main/java/hello/UserApplication.java
@@ -1,16 +1,15 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @RestController
@@ -29,7 +28,7 @@ public class UserApplication {
   @RequestMapping("/hi")
   public String hi(@RequestParam(value="name", defaultValue="Artaban") String name) {
     String greeting = this.restTemplate.getForObject("http://say-hello/greeting", String.class);
-    return String.format("%s, %s!", greeting, name);
+    return greeting;
   }
 
   public static void main(String[] args) {
